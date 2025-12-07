6e384f7dd808127df6a982f20afc99824da60fbd
andrebadrous <andre.badrous01@gmail.com>
2025-12-07 12:09:11 -0800

    Make fcfs run the processes in the order they came
    The add function in schedule_fcfs will place nodes
    at the head of the linked list
    
    The schedule function will take the node that came
    first, in this case the node at the tail, schedule
    and deleted it then repeat until there are no more
    processes to schedule

--- cecs_326/project_4/schedule_fcfs.c
+++ cecs_326/project_4/schedule_fcfs.c
@@ -1,30 +1,37 @@
 /**
  * FCFS scheduling
  */
- 
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "task.h"
 #include "list.h"
 #include "cpu.h"
 
-//Your code and design here
+// Your code and design here
+struct node *head;
 
-int main() {
-    return 0;
+void add(char *name, int priority, int burst)
+{
+    Task *task = malloc(sizeof(Task));
+    task->name = name;
+    task->priority = priority;
+    task->burst = burst;
 
+    insert(&head, task);
+}
 
+void schedule()
+{
+    struct node *temp;
 
-
-
-
-
-
-
-
-
-
-
-    
+    while (head != NULL)
+    {
+        temp = traverse(head, 0);
+        printf("Process [%s] With priority [%d] ran for [%d] ms.\n", temp->task->name, temp->task->priority, temp->task->burst);
+        usleep(temp->task->burst * 1000);
+        delete(&head, temp->task);
+    }
 }
